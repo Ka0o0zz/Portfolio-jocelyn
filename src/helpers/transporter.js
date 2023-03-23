58b8eb4f424a3e96e1e37b7031faf490f2b763b7
David Morales <davidbate5025@gmail.com>
2023-03-23 03:29:41 -0500

    cambios

--- src/helpers/transporter.js
+++ src/helpers/transporter.js
@@ -2,15 +2,37 @@ import nodemailer from "nodemailer";
 import hbs from "nodemailer-handlebars";
 import path from "path";
 
-let options = {
-  viewEngine: {
-    extname: ".hbs",
-    layoutsDir: path.resolve(__dirname, "../views/"),
-    defaultLayout: "",
-  },
-  viewPath: path.resolve(__dirname, "../views/"),
-  extName: ".hbs",
-};
+// let options = {
+//   viewEngine: {
+//     extname: ".hbs",
+//     layoutsDir: path.resolve(__dirname, "../views/"),
+//     defaultLayout: "",
+//   },
+//   viewPath: path.resolve(__dirname, "../views/"),
+//   extName: ".hbs",
+// };
+
+// //Use template configuration
+// transporter.use("compile", hbs(options));
+
+// function sendMail(options) {
+//   let _op = options;
+//   _op["from"] = {
+//     name: "Joselin",
+//     address: "nehasada@hotmail.com",
+//   };
+//   return new Promise((resolve, reject) => {
+//     transporter.sendMail(_op, function (err, data) {
+//       if (err) {
+//         return reject(err);
+//       }
+
+//       return resolve(data);
+//     });
+//   });
+// }
+
+// export default transporter;
 
 let transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com",
@@ -25,24 +47,16 @@ let transporter = nodemailer.createTransport({
   },
 });
 
-//Use template configuration
-transporter.use("compile", hbs(options));
-
-function sendMail(options) {
-  let _op = options;
-  _op["from"] = {
-    name: "Joselin",
-    address: "nehasada@hotmail.com",
-  };
-  return new Promise((resolve, reject) => {
-    transporter.sendMail(_op, function (err, data) {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve(data);
+export const sendEmail = async (to, subject, text) => {
+  try {
+    await transporter.sendMail({
+      from: "nehasada@hotmail.com",
+      to: to,
+      subject: subject,
+      text: text,
     });
-  });
-}
-
-export default transporter;
+    console.log("Correo electrónico enviado con éxito");
+  } catch (error) {
+    console.error("Error al enviar el correo electrónico:", error);
+  }
+};
--- src/pages/api/send-email.js
+++ src/pages/api/send-email.js
@@ -1,29 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import transporter from "@/helpers/transporter";
+import { sendEmail } from "@/helpers/transporter";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { name, lastName, email, message } = req.body;
     const year = new Date().getFullYear();
 
     try {
-      const mailOptions = {
-        from: "nehasada@hotmail.com",
-        to: ["davidbate5025@gmail.com", "jocelynnvargas1019@gmail.com"],
-        subject: "New Message From Portfolio | Joselin",
-        template: "email",
-        context: { name, email, lastName, message, year },
-      };
-
-      console.log({ email });
-
-      transporter.sendMail(mailOptions, function (err, data) {
-        if (err) {
-          console.log(err);
-        } else {
-        }
-      });
+      await sendEmail(
+        ["davidbate5025@gmail.com", "jocelynnvargas1019@gmail.com"],
+        "New Message From Portfolio | Joselin",
+        `Name: ${name}, Last Name:${lastName}, Email: ${email}, Message:${message}`
+      );
       res.status(200).json({
         ok: true,
         name,
